f0ce05246ed88e2f22979384f6934b50737d70ab
mehrabFronto <mehrabd20@gmail.com>
2024-06-25 17:10:39 +0330

    feat: add and config tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='bg-secondary-800 h-screen flex items-center justify-center'>
+      <h2 className='text-primary-900 font-bold text-3xl'>App</h2>
+    </div>
+  );
 };
 
 export default App;
